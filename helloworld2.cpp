dd1fac22494b5b29a0193f8f009784f1eea74b2f
BBBbosen <BBBbosen@gmail.com>
2023-09-30 22:07:25 +0800

    这是我自己在笨的的仓库中创建的一个cpp文件，用来输出Hello World 和测试

---
+++ helloworld2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+
+    cout<<"这是我自己在笨的的仓库中创建的一个cpp文件，用来输出Hello World 和测试"<<endl;
+    cout<<"Hello World"<<endl;
+
+
+
+    return 0;
+}
